5cfb06fbdd01c61e1223b239c861878db27e4e7d
Arij.BOUDHINA <arij.boudhina@esprit.tn>
2022-11-09 15:09:25 +0100

    test

--- src/test/java/tn/esprit/rh/achat/test/UserServiceImplMockTest.java
+++ src/test/java/tn/esprit/rh/achat/test/UserServiceImplMockTest.java
@@ -8,16 +8,15 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 
 
-import org.junit.jupiter.api.Assertions;
+
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
