26b1e8e09fffdda141ccac4f8dcdbb3d12e07c7d
skander.mufti <skander.mufti1@esprit.tn>
2022-11-18 00:10:08 +0100

    c skan

--- src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
+++ src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
@@ -18,10 +18,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import lombok.Builder;
 @Entity
+@Builder
 @Getter
 @Setter
+
 @AllArgsConstructor
 @NoArgsConstructor
 public class Fournisseur implements Serializable {
